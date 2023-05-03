ab46736ac29576e833e01b2cc42fa1220a9af924
rob-bender <jvladosyans@gmail.com>
2023-05-03 09:42:51 -0700

    change room_leave

--- sql/pb/room_leave.sql
+++ sql/pb/room_leave.sql
@@ -4,11 +4,17 @@ CREATE OR REPLACE FUNCTION room_leave(_uidr character varying, _uidu character v
 AS $function$
 DECLARE
 	_r rooms;
+	_u users;
 BEGIN
 	SELECT *
 	FROM rooms
 	WHERE uid_room = _uidr
 	INTO _r;
+
+	SELECT *
+	FROM users
+	WHERE uid = _uidu
+	INTO _u;
 
 	IF _r.id ISNULL THEN
 		RAISE EXCEPTION 'комната с таким uid не существует';
@@ -18,6 +24,10 @@ BEGIN
 		UPDATE rooms SET member_count = member_count-1 WHERE uid_room = _uidr;
 	ELSE
 		UPDATE rooms SET member_count=0, is_works=FALSE WHERE uid_room = _uidr;
+	END IF;
+
+	IF _u.id ISNULL THEN
+		UPDATE orders SET status = 'Chat closed' WHERE uid_order = _uidr;
 	END IF;
 END;
 $function$
